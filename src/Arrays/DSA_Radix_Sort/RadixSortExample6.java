c300ac15b14b176259be1304a242149b72a67902
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-18 17:40:51 +0530

    DSA, Day 9, Array Radix Sort

--- src/Arrays/DSA_Radix_Sort/RadixSortExample6.java
+++ src/Arrays/DSA_Radix_Sort/RadixSortExample6.java
@@ -1,4 +1,69 @@
 package Arrays.DSA_Radix_Sort;
 
+
+// Numbers with Same Digits but Different Orders
+// [321, 123, 213, 132, 312, 231]
+// [123, 132, 213, 231, 312, 321]
+
+
 public class RadixSortExample6 {
+
+    public static void radixSort(int[] arr) {
+        int max = getMax(arr);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+    }
+
+    public static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i : arr)
+            if (i > max)
+                max = i;
+        return max;
+    }
+
+    public static void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        for (int i = 0; i < n; i++) {
+            int index = (arr[i] / exp) % 10;
+            count[index]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int index = (arr[i] / exp) % 10;
+            output[count[index] - 1] = arr[i];
+            count[index]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {321, 123, 213, 132, 312, 231};
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        radixSort(arr);
+
+        System.out.println("After Radix Sort:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr)
+            System.out.print(i + " ");
+        System.out.println();
+    }
 }
