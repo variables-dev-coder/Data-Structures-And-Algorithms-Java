6dc5c3144938a4b0e9ca8048547d23ea6b989a30
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-18 17:51:07 +0530

    DSA, Day 9, Array Radix Sort, Array with all elements same, Program

---
+++ src/Arrays/DSA_Radix_Sort/RadixSortExample9.java
@@ -0,0 +1,73 @@
+package Arrays.DSA_Radix_Sort;
+
+
+// Array with All Elements Same
+// [999, 999, 999, 999, 999]
+// [999, 999, 999, 999, 999]
+
+
+public class RadixSortExample9 {
+
+    public static void radixSort(int[] arr) {
+        int max = getMax(arr);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+    }
+
+    public static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i : arr)
+            if (i > max)
+                max = i;
+        return max;
+    }
+
+    public static void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        // Count occurrences
+        for (int i = 0; i < n; i++) {
+            int digit = (arr[i] / exp) % 10;
+            count[digit]++;
+        }
+
+        // Accumulate counts
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Build output array (stable sort)
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = (arr[i] / exp) % 10;
+            output[count[digit] - 1] = arr[i];
+            count[digit]--;
+        }
+
+        // Copy to original array
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {999, 999, 999, 999, 999};
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        radixSort(arr);
+
+        System.out.println("After Radix Sort:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+}
