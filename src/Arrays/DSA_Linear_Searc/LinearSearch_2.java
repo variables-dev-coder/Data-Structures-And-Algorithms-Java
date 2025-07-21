0b4244aa620ba4582644e3136260d3491365eb26
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-21 20:31:11 +0530

    DSA, Day 11, Array, Linear Search Program

---
+++ src/Arrays/DSA_Linear_Searc/LinearSearch_2.java
@@ -0,0 +1,28 @@
+package Arrays.DSA_Linear_Searc;
+
+
+// Find the index of the element in the array. If not found, return -1.
+
+
+public class LinearSearch_2 {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;   // element fount
+            }
+        }
+        return -1;   // Element not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60};
+        int target = 30;
+
+        int result = linearSearch(arr, target);
+
+        if (result != -1)   // or == target
+            System.out.println("Element Found at index: " + result);
+        else
+            System.out.println("Element not Found!");
+    }
+}
