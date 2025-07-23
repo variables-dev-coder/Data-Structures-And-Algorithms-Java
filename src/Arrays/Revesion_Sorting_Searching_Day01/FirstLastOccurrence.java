1505411534cc4a53009a738be5b7cfb716de1066
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-23 17:57:39 +0530

    DSA, Day 13, Array Rivision Return first/last occurrence index of a target element. Program

--- src/Arrays/Revesion_Sorting_Searching_Day01/FirstLastOccurrence.java
+++ src/Arrays/Revesion_Sorting_Searching_Day01/FirstLastOccurrence.java
@@ -1,4 +1,21 @@
 package Arrays.Revesion_Sorting_Searching_Day01;
 
 public class FirstLastOccurrence {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 2, 3, 7, 3, 9, 10};
+        int target = 3;
+        int first = -1;
+        int last = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                if (first == -1)
+                    first = i;
+                last = i;
+            }
+        }
+
+        System.out.println("First Occurrence of " + target + " = " + first);
+        System.out.println("Last Occurrence of " + target + " = " + last);
+    }
 }
