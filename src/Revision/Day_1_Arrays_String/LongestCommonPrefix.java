956bdc159213985c07c551198dcf154f8d063fdf
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-03 15:04:58 +0530

    DSA, String Revision Longest Common Prefix

---
+++ src/Revision/Day_1_Arrays_String/LongestCommonPrefix.java
@@ -0,0 +1,21 @@
+package Revision.Day_1_Arrays_String;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"flower", "flow", "flight"};
+        System.out.println("LCP: " + longestCommonPrefix(arr));
+    }
+}
