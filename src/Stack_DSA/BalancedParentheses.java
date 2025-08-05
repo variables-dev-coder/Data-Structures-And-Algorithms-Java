28874377a25906b362607b9c80f2fe51c578da5c
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-05 23:12:57 +0530

    DSA, Day 22, Stack Problem

---
+++ src/Stack_DSA/BalancedParentheses.java
@@ -0,0 +1,31 @@
+package Stack_DSA;
+
+
+// Balanced Parentheses
+
+import java.util.Stack;
+
+public class BalancedParentheses {
+    public static boolean isBalanced(String expr) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : expr.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                        (ch == '}' && top != '{') ||
+                        (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String input = "{[()]}";
+        System.out.println("Balanced? " + isBalanced(input)); // true
+    }
+}
