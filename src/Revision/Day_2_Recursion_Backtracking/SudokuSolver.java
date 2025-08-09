424ddd024e2fbb70e42a7dae045224f7a82658b7
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-09 11:06:38 +0530

    DSA, Saturday revision Suduku Solver example

---
+++ src/Revision/Day_2_Recursion_Backtracking/SudokuSolver.java
@@ -0,0 +1,92 @@
+package Revision.Day_2_Recursion_Backtracking;
+
+public class SudokuSolver {
+    public static void main(String[] args) {
+        int[][] board = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists");
+        }
+    }
+
+    static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) { // Empty cell
+                    for (int num = 1; num <= 9; num++) {
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solveSudoku(board)) {
+                                return true;
+                            }
+
+                            // Backtrack
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false; // No valid number found
+                }
+            }
+        }
+        return true; // Solved
+    }
+
+    static boolean isSafe(int[][] board, int row, int col, int num) {
+        // Row check
+        for (int x = 0; x < 9; x++) {
+            if (board[row][x] == num) return false;
+        }
+
+        // Column check
+        for (int x = 0; x < 9; x++) {
+            if (board[x][col] == num) return false;
+        }
+
+        // 3×3 box check
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num) return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void printBoard(int[][] board) {
+        for (int r = 0; r < 9; r++) {
+            for (int d = 0; d < 9; d++) {
+                System.out.print(board[r][d] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+5 3 4 6 7 8 9 1 2
+6 7 2 1 9 5 3 4 8
+1 9 8 3 4 2 5 6 7
+8 5 9 7 6 1 4 2 3
+4 2 6 8 5 3 7 9 1
+7 1 3 9 2 4 8 5 6
+9 6 1 5 3 7 2 8 4 
+2 8 7 4 1 9 6 3 5
+3 4 5 2 8 6 1 7 9
+
+ */
