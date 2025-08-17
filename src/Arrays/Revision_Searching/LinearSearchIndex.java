348a789c4adf9fe212007b3efdc522183a2cac43
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-17 16:35:11 +0530

    DSA, Day 31, Linear Search First Occurrence example

---
+++ src/Arrays/Revision_Searching/LinearSearchIndex.java
@@ -0,0 +1,25 @@
+package Arrays.Revision_Searching;
+
+public class LinearSearchIndex {
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;    // return index of first occurrence
+            }
+        }
+        return -1;   // if not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 56, 78, 70, 23};
+        int key = 34;
+
+        int index = linearSearch(arr, key);
+
+        if (index != -1) {
+            System.out.println("Element " + key + " found at index " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
