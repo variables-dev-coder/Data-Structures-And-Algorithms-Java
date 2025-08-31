e4cc59353b306e06bc0ca1d9e253ab869c0e8d74
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-31 22:26:28 +0530

    DSA, Day 44 Stack problem

---
+++ src/Revision/Day_4_Stack/MinStack.java
@@ -0,0 +1,51 @@
+package Revision.Day_4_Stack;
+
+import java.util.Stack;
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) return;
+        int removed = stack.pop();
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println("Min: " + minStack.getMin()); // -3
+        minStack.pop();
+        System.out.println("Top: " + minStack.top()); // 0
+        System.out.println("Min: " + minStack.getMin()); // -2
+    }
+}
+
+//Min: -3
+//Top: 0
+//Min: -2
