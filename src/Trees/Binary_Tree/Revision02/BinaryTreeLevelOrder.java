baac0047becbd1c220d8ef4c883452a23ef77a7a
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-09-15 23:59:23 +0530

    DSA, Day 53, Binary Tree Level Order example

---
+++ src/Trees/Binary_Tree/Revision02/BinaryTreeLevelOrder.java
@@ -0,0 +1,58 @@
+package Trees.Binary_Tree.Revision02;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrder {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    Node root;
+
+    void levelOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            System.out.print(current.data + " ");
+
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeLevelOrder tree = new BinaryTreeLevelOrder();
+
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+
+        System.out.println("Level Order Traversal: ");
+        tree.levelOrder(tree.root);
+    }
+}
+
