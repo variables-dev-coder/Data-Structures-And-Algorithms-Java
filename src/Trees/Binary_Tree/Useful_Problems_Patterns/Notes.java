9d606ad43630c107ffdabe561f6ef26e67f46df5
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-10-09 10:28:08 +0530

    DSA Day 64, Binary Tree day 10, useful Problem and Patterns Notes

---
+++ src/Trees/Binary_Tree/Useful_Problems_Patterns/Notes.java
@@ -0,0 +1,19 @@
+package Trees.Binary_Tree.Useful_Problems_Patterns;
+
+public class Notes {
+    public static void main(String[] args) {
+
+        /*
+
+I will go one by one with:
+    1 Lowest Common Ancestor (LCA)
+    2 Diameter of Tree
+    3 Path Sum / Root-to-Leaf Paths
+    4 Level-Order Zigzag & Boundary Traversal
+    5 Serialize / Deserialize Tree
+    6 Convert Sorted Array → Balanced BST
+
+
+         */
+    }
+}
