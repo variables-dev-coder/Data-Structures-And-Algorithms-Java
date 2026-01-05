e241787ddc965e516d44ea63eb0accbb9b2864ec
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-05 16:46:59 +0530

    DSA, Day 80, Binary Tree Views and Properties Day 1, Program 1: Level Order Traversal (BFS)

---
+++ src/Trees/Binary_Tree_Views_Properties/day01_Foundation_TraversalControl/LevelOrderTraversal.java
@@ -0,0 +1,54 @@
+package Trees.Binary_Tree_Views_Properties.day01_Foundation_TraversalControl;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            result.add(level);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println(levelOrder(root));
+    }
+}
+
+// [[1], [2, 3], [4, 5]]
