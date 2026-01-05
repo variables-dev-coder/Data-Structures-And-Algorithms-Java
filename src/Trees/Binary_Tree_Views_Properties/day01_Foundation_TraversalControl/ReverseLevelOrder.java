5ac3f973885209f271e4df80ff82c4a06132456e
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-05 16:52:35 +0530

    DSA, Day 80, Binary Tree Views and Properties Day 1, Program 3: Reverse Level Order Traversal

--- src/Trees/Binary_Tree_Views_Properties/day01_Foundation_TraversalControl/ReverseLevelOrder.java
+++ src/Trees/Binary_Tree_Views_Properties/day01_Foundation_TraversalControl/ReverseLevelOrder.java
@@ -1,9 +1,11 @@
 package Trees.Binary_Tree_Views_Properties.day01_Foundation_TraversalControl;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
-public class LevelOrderByLine {
+public class ReverseLevelOrder {
     static class TreeNode {
         int val;
         TreeNode left, right;
@@ -13,37 +15,38 @@ public class LevelOrderByLine {
         }
     }
 
-    public static void printLevelByLine(TreeNode root) {
-        if (root == null) return;
+    public static List<List<Integer>> reverseLevelOrder(TreeNode root) {
+        LinkedList<List<Integer>> result = new LinkedList<>();
+        if (root == null) return result;
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
         while (!queue.isEmpty()) {
             int size = queue.size();
+            List<Integer> level = new ArrayList<>();
 
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
-                System.out.print(node.val + " ");
+                level.add(node.val);
 
                 if (node.left != null) queue.offer(node.left);
                 if (node.right != null) queue.offer(node.right);
             }
-            System.out.println();
+            result.addFirst(level); // 🔥 important
         }
+        return result;
     }
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(10);
-        root.left = new TreeNode(20);
-        root.right = new TreeNode(30);
-        root.left.left = new TreeNode(40);
-        root.right.right = new TreeNode(50);
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
 
-        printLevelByLine(root);
+        System.out.println(reverseLevelOrder(root));
     }
 }
 
-//10
-//20 30
-//40 50
+//[[4, 5], [2, 3], [1]]
