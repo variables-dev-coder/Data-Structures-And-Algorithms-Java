d66cf48426c3ad53c38233b4b041f2661a804fce
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-07 23:10:57 +0530

    DSA Day 82, Deque, Spiral Traversal

---
+++ src/Trees/Binary_Tree_Views_Properties/day03_Zigzag_Spiral_Traversal/SpiralTraversalDeque.java
@@ -0,0 +1,58 @@
+package Trees.Binary_Tree_Views_Properties.day03_Zigzag_Spiral_Traversal;
+
+import java.util.*;
+
+public class SpiralTraversalDeque {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int val) { this.val = val; }
+    }
+
+    public static List<List<Integer>> spiralTraversal(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Deque<Integer> level = new LinkedList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (leftToRight) {
+                    level.addLast(node.val);
+                } else {
+                    level.addFirst(node.val);
+                }
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            result.add(new ArrayList<>(level));
+            leftToRight = !leftToRight;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(spiralTraversal(root));
+    }
+}
+
+// [[1], [3, 2], [4, 5, 6, 7]]
