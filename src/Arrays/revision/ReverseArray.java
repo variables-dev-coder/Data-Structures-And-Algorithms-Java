1571cbad79e60e56fe872bfac80818ed22092da1
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-12 21:58:08 +0530

    Array revision, Reverse an Array

---
+++ src/Arrays/revision/ReverseArray.java
@@ -0,0 +1,23 @@
+package Arrays.revision;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
