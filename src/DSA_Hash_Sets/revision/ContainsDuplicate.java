cb376467676de7516f9ae424d1cbd2d366aa100e
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-13 23:46:28 +0530

    Hash Table revision, Contains Duplicate (LC 217)

---
+++ src/DSA_Hash_Sets/revision/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+package DSA_Hash_Sets.revision;
+
+import java.util.*;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            if (!set.add(n)) {
+                System.out.println(true);
+                return;
+            }
+        }
+        System.out.println(false);
+    }
+}
+
