46e29580857bd21a8978cff8fa5c051a9f709444
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-19 17:38:49 +0530

    Check if Two Strings Are Anagrams

---
+++ src/HASH_TABLES/revision/AnagramDemo.java
@@ -0,0 +1,27 @@
+package HASH_TABLES.revision;
+
+import java.util.*;
+
+public class AnagramDemo {
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0)
+                return false;
+            map.put(c, map.get(c) - 1);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent"));
+    }
+}
+
