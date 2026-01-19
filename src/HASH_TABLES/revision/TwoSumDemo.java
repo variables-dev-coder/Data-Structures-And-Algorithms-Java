d0d848c3154e061e9c4d80653785d10fc048fdd7
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-01-19 17:32:39 +0530

    Two Sum

---
+++ src/HASH_TABLES/revision/TwoSumDemo.java
@@ -0,0 +1,26 @@
+package HASH_TABLES.revision;
+
+import java.util.*;
+
+public class TwoSumDemo {
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+    }
+}
+
