223e1fbf531c305966051b8949d6e0c0387f6e8d
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-05 19:36:12 +0530

    DSA Day 95, Tree construction, Vertical Order Traversal

---
+++ src/Trees/Tree_Construction/day08_Top_Bottom_Vertical_Views/VerticalOrderTraversal.java
@@ -0,0 +1,52 @@
+package Trees.Tree_Construction.day08_Top_Bottom_Vertical_Views;
+
+import java.util.*;
+
+public class VerticalOrderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int v) { val = v; }
+    }
+
+    static class Pair {
+        TreeNode node;
+        int hd;
+        Pair(TreeNode n, int h) { node = n; hd = h; }
+    }
+
+    public static List<List<Integer>> verticalOrder(TreeNode root) {
+        Map<Integer, List<Integer>> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+
+        q.offer(new Pair(root, 0));
+
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
+
+            map.putIfAbsent(p.hd, new ArrayList<>());
+            map.get(p.hd).add(p.node.val);
+
+            if (p.node.left != null)
+                q.offer(new Pair(p.node.left, p.hd - 1));
+            if (p.node.right != null)
+                q.offer(new Pair(p.node.right, p.hd + 1));
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        root.left.right.right = new TreeNode(7);
+
+        System.out.println("Vertical Order: " + verticalOrder(root));
+    }
+}
+
