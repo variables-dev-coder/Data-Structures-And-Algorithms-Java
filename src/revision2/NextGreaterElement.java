e86e1cb07b6ba9a2cb65b2718611b52dd128fb87
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-02-23 22:37:13 +0530

    Monotonic Stack – Next Greater Element

---
+++ src/revision2/NextGreaterElement.java
@@ -0,0 +1,33 @@
+package revision2;
+
+import java.util.*;
+
+public class NextGreaterElement {
+
+    public static int[] nextGreater(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            while(!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 2, 10};
+
+        int[] result = nextGreater(arr);
+
+        System.out.println("Next Greater Elements:");
+        System.out.println(Arrays.toString(result));
+    }
+}
