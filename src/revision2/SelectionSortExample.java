d49d01e2d79afee1a34865b8354e0e31546ad4fb
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-15 19:18:04 +0530

    Selection sort example

---
+++ src/revision2/SelectionSortExample.java
@@ -0,0 +1,36 @@
+package revision2;
+
+import java.util.*;
+
+public class SelectionSortExample {
+
+    static void selectionSort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
