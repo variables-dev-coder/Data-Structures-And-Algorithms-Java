ca91b5946da1b2eb353afdf26b5fb9171b515aae
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-16 22:34:39 +0530

    Merge sort example

---
+++ src/revision3/MergeSortExample.java
@@ -0,0 +1,62 @@
+package revision3;
+
+public class MergeSortExample {
+
+    public static void mergeSort(int[] arr, int left, int right) {
+
+        if(left < right) {
+
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for(int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for(int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while(i < n1 && j < n2) {
+
+            if(L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+
+        }
+
+        while(i < n1)
+            arr[k++] = L[i++];
+
+        while(j < n2)
+            arr[k++] = R[j++];
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5,3,8,4,2};
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
