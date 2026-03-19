bdce64ce2531e5b25d5d592c9de0ad0d5eb1baa6
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-19 11:46:47 +0530

    Binary Tree example

---
+++ src/Trees/treeRevision/BinaryTreeExample.java
@@ -0,0 +1,39 @@
+package Trees.treeRevision;
+
+class BinaryTreeExample {
+
+    // Node structure
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    // Inorder Traversal (Left → Root → Right)
+    static void inorder(Node root) {
+        if (root == null) return;
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        // Create nodes
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.print("Inorder Traversal: ");
+        inorder(root);
+    }
+}
