f4556ac1bf5bdc202b4e69522ad56ceaf0670322
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-20 14:15:49 +0530

    Binary Tree, Height of Tree

---
+++ src/Trees/treeRevision/HeightTree.java
@@ -0,0 +1,21 @@
+package Trees.treeRevision;
+
+class HeightTree {
+    static class Node {
+        int val;
+        Node left, right;
+        Node(int v) { val = v; }
+    }
+
+    static int height(Node root) {
+        if (root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+
+        System.out.println(height(root)); // 2
+    }
+}
