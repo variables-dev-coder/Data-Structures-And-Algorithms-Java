7e95807f4a4f81484ee4f9fdac1daba7d703637a
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-23 17:19:13 +0530

    Hash Table, Group Anagrams

---
+++ src/HASH_TABLES/revision/GroupAnagrams.java
@@ -0,0 +1,31 @@
+package HASH_TABLES.revision;
+
+import java.util.*;
+
+class GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+
+        System.out.println(groupAnagrams(strs));
+    }
+}
