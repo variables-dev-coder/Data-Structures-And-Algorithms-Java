1aa3037c822efdde34937d99f63f5cf90a016c0b
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-23 17:06:09 +0530

    Hash Table, Two Sum example

---
+++ src/HASH_TABLES/revision/TwoSum.java
@@ -0,0 +1,31 @@
+package HASH_TABLES.revision;
+
+import java.util.*;
+
+class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        System.out.println(Arrays.toString(twoSum(nums, target))); // [0,1]
+    }
+}
