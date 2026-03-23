c4aabb8a35114f7a98608682008321eda8662479
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-23 17:12:40 +0530

    Hash Table, Subarray Sum Equals K

---
+++ src/HASH_TABLES/revision/SubarraySum.java
@@ -0,0 +1,35 @@
+package HASH_TABLES.revision;
+
+import java.util.*;
+
+class SubarraySum {
+
+    public static int subarraySum(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int sum = 0, count = 0;
+
+        for (int num : nums) {
+
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,1,1};
+        int k = 2;
+
+        System.out.println(subarraySum(nums, k)); // 2
+    }
+}
