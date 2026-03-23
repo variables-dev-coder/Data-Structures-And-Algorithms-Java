5b200e61938af93bd5c5fd3e577e756555e2ae7b
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-23 17:20:20 +0530

    Hash table, Count Subarrays with Equal 0s and 1s

---
+++ src/HASH_TABLES/revision/EqualZeroOne.java
@@ -0,0 +1,36 @@
+package HASH_TABLES.revision;
+
+import java.util.*;
+
+class EqualZeroOne {
+
+    public static int countSubarrays(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int sum = 0, count = 0;
+
+        for (int num : nums) {
+
+            if (num == 0) num = -1;
+
+            sum += num;
+
+            if (map.containsKey(sum)) {
+                count += map.get(sum);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {0,1,0};
+
+        System.out.println(countSubarrays(nums)); // 2
+    }
+}
