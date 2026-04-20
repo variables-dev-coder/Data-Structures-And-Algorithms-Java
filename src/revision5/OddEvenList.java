34d6ee2066b53fc12dbc2dafef8d5610c2770b5c
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-04-20 21:14:57 +0530

    Added Odd Even Linked List (LeetCode #328)

---
+++ src/revision5/OddEvenList.java
@@ -0,0 +1,54 @@
+package revision5;
+
+class OddEvenList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+    }
+
+    public static ListNode oddEvenList(ListNode head) {
+
+        if (head == null) return null;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return head;
+    }
+
+    public static void print(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        // 1 → 2 → 3 → 4 → 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        head = oddEvenList(head);
+
+        print(head);
+    }
+}
