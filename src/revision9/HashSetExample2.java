d5db50c7e51102ba600ae82b6552b9d31c1e0be5
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-06-10 11:42:57 +0530

    Added Check Element Exists

---
+++ src/revision9/HashSetExample2.java
@@ -0,0 +1,17 @@
+package revision9;
+
+import java.util.HashSet;
+
+public class HashSetExample2 {
+    public static void main(String[] args) {
+
+        HashSet<Integer> numbers = new HashSet<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        System.out.println(numbers.contains(20));
+        System.out.println(numbers.contains(50));
+    }
+}
