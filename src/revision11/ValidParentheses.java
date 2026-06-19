00275dd3cfdfccc2ab90619a044bd13d145c6f8d
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-06-19 21:34:11 +0530

    Added Valid Parentheses (LC 20)

---
+++ src/revision11/ValidParentheses.java
@@ -0,0 +1,38 @@
+package revision11;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()) {
+
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            else {
+
+                if(stack.isEmpty())
+                    return false;
+
+                char top = stack.pop();
+
+                if(ch == ')' && top != '(') return false;
+                if(ch == '}' && top != '{') return false;
+                if(ch == ']' && top != '[') return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                isValid("()[]{}")
+        );
+    }
+}
