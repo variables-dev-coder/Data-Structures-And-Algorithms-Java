943275fe119dbe9f400396f296be0e12320e6a97
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-06-20 10:02:28 +0530

    Added Stock Span Problem

---
+++ src/revision11/StockSpan.java
@@ -0,0 +1,37 @@
+package revision11;
+
+import java.util.Stack;
+
+public class StockSpan {
+
+    public static void main(String[] args) {
+
+        int[] price = {100,80,60,70,60,75,85};
+        int n = price.length;
+
+        int[] span = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < n; i++) {
+
+            while(!stack.isEmpty() &&
+                    price[stack.peek()] <= price[i]) {
+
+                stack.pop();
+            }
+
+            if(stack.isEmpty()) {
+                span[i] = i + 1;
+            } else {
+                span[i] = i - stack.peek();
+            }
+
+            stack.push(i);
+        }
+
+        for(int s : span) {
+            System.out.print(s + " ");
+        }
+    }
+}
