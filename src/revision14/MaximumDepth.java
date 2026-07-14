971db0e0587453347fc3716be272bbbb89cbb8b7
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-07-14 19:52:47 +0530

    LC 104 - Maximum Depth of Binary Tree

---
+++ src/revision14/MaximumDepth.java
@@ -0,0 +1,38 @@
+package revision14;
+
+class TreeNode {
+
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int data) {
+        this.data = data;
+    }
+}
+
+public class MaximumDepth {
+
+    public static int maxDepth(TreeNode root) {
+
+        if (root == null)
+            return 0;
+
+        return Math.max(maxDepth(root.left),
+                maxDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+
+        root.left = new TreeNode(9);
+
+        root.right = new TreeNode(20);
+
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(maxDepth(root));
+    }
+}
