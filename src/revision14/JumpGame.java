79092739a169fb143ab2e5305b4092ea901e6189
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-07-15 16:52:48 +0530

    LC 55 - Jump Game

---
+++ src/revision14/JumpGame.java
@@ -0,0 +1,26 @@
+package revision14;
+
+public class JumpGame {
+
+    public static boolean canJump(int[] nums) {
+
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > maxReach)
+                return false;
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2,3,1,1,4};
+
+        System.out.println(canJump(nums));
+    }
+}
